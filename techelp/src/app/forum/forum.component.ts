f46614fdc20037468261e9a192cb18079906269d
Lucas0092 <lucasumc@outlook.com>
2021-09-12 22:54:09 -0300

    componente forum e resposta atualizados

--- techelp/src/app/forum/forum.component.ts
+++ techelp/src/app/forum/forum.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Forum } from '../model/Forum';
+import { ForumService } from '../service/forum.service';
 
 @Component({
   selector: 'app-forum',
@@ -8,15 +11,38 @@ import { Router } from '@angular/router';
 })
 export class ForumComponent implements OnInit {
 
+  forum: Forum = new Forum()
+  listaForum: Forum[]
+
+
   constructor(
-    private router: Router
+    private router: Router,
+    private forumService: ForumService
   ) { }
 
   ngOnInit() {
+
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
+    }
+
+    this.findAllForum()
+
   }
 
-  forum(){
-    this.router.navigate(["/forum"])
+
+  findAllForum() {
+    this.forumService.getAllForum().subscribe((resp: Forum[]) => {
+      this.listaForum = resp
+    })
+  }
+
+  cadastrar() {
+    this.forumService.postForum(this.forum).subscribe((resp: Forum) => {
+      this.forum = resp
+      alert('cadastrado com sucesso')
+      this.forum = new Forum()
+    })
   }
 
 
--- techelp/src/app/resposta/resposta.component.ts
+++ techelp/src/app/resposta/resposta.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Forum } from '../model/Forum';
+import { Resposta } from '../model/Resposta';
+import { ForumService } from '../service/forum.service';
+import { RespostaService } from '../service/resposta.service';
 
 @Component({
   selector: 'app-resposta',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RespostaComponent implements OnInit {
 
-  constructor() { }
+  resposta: Resposta = new Resposta()
+  listaResposta: Resposta[]
 
-  ngOnInit(): void {
+  forum: Forum = new Forum()
+  listaForum: Forum[]
+
+   
+
+  constructor(
+    private router: Router,
+    private respostaService: RespostaService,
+    private forumService: ForumService,
+  ) { }
+
+  ngOnInit(){
+
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
+    }
+
+  }
+
+  getAllResposta(){
+    this.respostaService.getAllResposta().subscribe((resp: Resposta[]) => {
+      this.listaResposta = resp
+    })
+  }
+
+  getAllForum(){
+    this.forumService.getAllForum().subscribe((resp: Forum[]) => {
+      this.listaForum = resp
+    })
   }
 
 }
